2f3b524d29c73f24313d889360a9a6e1688d6917
Fatima Alhusain <justanlaptop12@gmail.com>
2025-09-16 00:09:29 +0300

    Add files via upload

---
+++ BlogDataLibrary/Models/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlogDataLibrary.Models
+{
+    public class UserModel
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
